8741fb1cac9000f6d1b18039cb909f05076e1c37
Ipsita2303 <ipsitagupta798@gmail.com>
2026-03-06 01:06:56 +0530

    Made few changes in the code app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
 from utils import extract_text_from_pdf, rank_resumes
 
@@ -6,23 +6,27 @@ st.title("AI Resume Ranking System")
 
 st.write("Upload resumes and rank them according to job description")
 
-job_description = st.text_area(
-    "Enter Job Description"
-)
+# JOB DESCRIPTION INPUT
+job_description = st.text_area("Enter Job Description")
 
-upload_files = st.file_uploader(
-    "Upload Resumes(PDF)",
-    accept_multiple_files = True,
+# FILE UPLOAD
+uploaded_files = st.file_uploader(
+    "Upload Resumes (PDF)",
+    accept_multiple_files=True,
     type=["pdf"]
 )
 
+# BUTTON
 if st.button("Rank Resumes"):
+
     if job_description == "":
         st.warning("Please enter job description")
 
     elif uploaded_files is None:
         st.warning("Please upload resumes")
+
     else:
+
         resume_texts = []
         resume_names = []
 
@@ -32,14 +36,15 @@ if st.button("Rank Resumes"):
             resume_names.append(file.name)
 
         scores = rank_resumes(job_description, resume_texts)
+
         data = pd.DataFrame({
             "Resume": resume_names,
             "Score": scores
         })
-        data = data.sort_values(
-            by="Score",
-            ascending=False
-        )
+
+        data = data.sort_values(by="Score", ascending=False)
+
         st.subheader("Ranking Results")
         st.dataframe(data)
+
         st.bar_chart(data.set_index("Resume"))
