22602daeed275a5b775238b92e6b8a94a5bbb501
Ipsita2303 <ipsitagupta798@gmail.com>
2026-03-06 01:25:24 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,15 +1,19 @@
-import os
-from openai import OpenAI
-import numpy as np
 import PyPDF2
+from openai import OpenAI
+from sklearn.metrics.pairwise import cosine_similarity
+import os
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-def extract_text_from_pdf(file):
-    reader = PyPDF2.PdfReader(file)
+
+def extract_text_from_pdf(uploaded_file):
     text = ""
-    for page in reader.pages:
-        text += page.extract_text()
+    pdf = PyPDF2.PdfReader(uploaded_file)
+
+    for page in pdf.pages:
+        if page.extract_text():
+            text += page.extract_text()
+
     return text
 
 
@@ -18,23 +22,22 @@ def get_embedding(text):
         model="text-embedding-3-small",
         input=text
     )
+
     return response.data[0].embedding
 
 
-def cosine_similarity(a, b):
-    a = np.array(a)
-    b = np.array(b)
-    return np.dot(a,b) / (np.linalg.norm(a) * np.linalg.norm(b))
+def rank_resumes(job_desc, resumes):
 
+    job_embedding = get_embedding(job_desc)
 
-def rank_resumes(job_description, resume_texts):
-    job_embedding = get_embedding(job_description)
+    resume_embeddings = []
 
-    scores = []
+    for resume in resumes:
+        resume_embeddings.append(get_embedding(resume))
 
-    for text in resume_texts:
-        resume_embedding = get_embedding(text)
-        score = cosine_similarity(job_embedding, resume_embedding)
-        scores.append(score)
+    scores = cosine_similarity(
+        [job_embedding],
+        resume_embeddings
+    )[0]
 
     return scores
