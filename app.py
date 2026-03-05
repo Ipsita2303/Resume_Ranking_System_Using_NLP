1773e2cc007d80b214c29fb14816a1b5318ad7dd
Ipsita2303 <ipsitagupta798@gmail.com>
2026-03-06 00:47:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st 
+import pandas as pd
+from utils import extract_text_from_, rank_resumes
+
+st.title("AI Resume Ranking System")
+
+st.write("Upload resumes and rank them according to job description")
+
+job_description = st.text_area(
+    "Enter Job Description"
+)
+
+upload_files = st.file_uploader(
+    "Upload Resumes(PDF)",
+    accept_multiple_files = True,
+    type=["pdf"]
+)
+
+if st.button("Rank Resumes"):
+    if job_description == "":
+        st.warning("Please enter job description")
+
+    elif uploaded_files is None:
+        st.warning("Please upload resumes")
+    else:
+        resume_texts = []
+        resume_names = []
+
+        for file in uploaded_files:
+            text = extract_text_from_pdf(file)
+            resume_texts.append(text)
+            resume_names.append(file.name)
+
+        scores = rank_resumes(job_description, resume_texts)
+        data = pd.DataFrame({
+            "Resume": resume_names,
+            "Score": scores
+        })
+        data = data.sort_values(
+            by="Score",
+            ascending=False
+        )
+        st.subheader("Ranking Results")
+        st.dataframe(data)
+        st.bar_chart(data.set_index("Resume"))
